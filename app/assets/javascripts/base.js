48995bc678cf43fedef1ac4f120ce117b72f798d
augustoam <augusto.albertoni@gmail.com>
2017-12-05 19:51:22 -0200

    Job para conexão com o broker e atualização do time

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -1,29 +1,28 @@
 $(document).on('turbolinks:load', function() {
   title.innerHTML = "Timer client one";
 
-  window.setInterval('publish_time_mqtt()', 10000);
-
-  function time() {
-    var time = new Date();
-    var n = time.toLocaleTimeString();
-    document.getElementById("time").innerHTML = time.toLocaleTimeString();
-  }
-  setInterval(time, 1000);
+  // function time() {
+  //   var time = new Date();
+  //   var n = time.toLocaleTimeString();
+  //   document.getElementById("time").innerHTML = time.toLocaleTimeString();
+  // }
+  // setInterval(time, 1000);
+  //
+  setInterval('publish_time_mqtt()', 1000);
 
   $.get({
     url: "time/get_time_now"
   }).done(function(data) {
     console.log(data);
     document.getElementById("time").innerHTML = data;
-    console.log(data);
   });
-
 
 })
 
 function publish_time_mqtt() {
 
-  thisTime = document.getElementById("time").val;
+  var thisTime = $('#time').get(0).textContent;
+  console.log(thisTime);
   var vData = {
     time: thisTime
   };
--- app/controllers/time_controller.rb
+++ app/controllers/time_controller.rb
@@ -1,21 +1,11 @@
 class TimeController < ActionController::API
   include MqttBroker
 
-  def get_time
-    render json: { time: Time.now }
-  end
-
-  def set_time
-    # debugger
-    Timer.create!(time: params[:time])
-    render json: :ok, status: :ok
-  end
-
-  def get_time_now
-    render json: Timer.last.time
-  end
-
   def publish_time_mqtt
-    publish_mqtt('uri/sistemas-distribuidos/time', params[:time])
+    publish_mqtt('uri/sistemas-distribuidos/time', 'one ' + params[:time].to_s)
+  end
+
+  def get_time
+    Timer.last
   end
 end
